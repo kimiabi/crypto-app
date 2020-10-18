782ef80d5f34313978585ee57d183b7ae3a05758
kimberly Gonzalez <kgonzalezm9@miumg.edu.gt>
2020-10-17 22:55:32 -0600

    commit initial

---
+++ precache-manifest.e1429eba5d28022e616a5381dc95e108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331269149ba50483169e",
+    "url": "/css/app.ad5ab6a5.css"
+  },
+  {
+    "revision": "38f522120f930e5d4e5d2517bf03a6f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "331269149ba50483169e",
+    "url": "/js/app.ffefb1b9.js"
+  },
+  {
+    "revision": "309255dd85b6225c53f0",
+    "url": "/js/chunk-vendors.38ae7b3f.js"
+  },
+  {
+    "revision": "941a542086b12dbf61dc6b9837f7601e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
